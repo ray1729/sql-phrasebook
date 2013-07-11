1eb7d5780473b00b3ce10cb5a2fc558a3cb4e945
Ray Miller <rm7@sanger.ac.uk>
2013-07-11 10:34:26 +0100

    Update project for release.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject uk.org.1729/sql-phrasebook "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject uk.org.1729/sql-phrasebook "0.1.0"
+  :description "An SQL phrasebook allowing you to collect queries in a shared dictionary rather than scattering them throughout your code"
+  :url "https://github.com/ray1729/sql-phrasebook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
